2670c83486d3705c53f11e507916b6962a757f78
furqanx <alghifarifurqan03@gmail.com>
2023-04-11 23:25:06 +0700

    Create connections

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'conversation_id',
+        'read',
+        'type',
+        'body'
+    ];
+
+    // relationships 
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
 }
