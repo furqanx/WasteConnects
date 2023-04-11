3e5baa54562d73416796069070cf8aa609f2d726
furqanx <alghifarifurqan03@gmail.com>
2023-04-11 23:24:31 +0700

    Create databases column

--- database/migrations/2023_04_11_094435_create_conversations_table.php
+++ database/migrations/2023_04_11_094435_create_conversations_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('conversations', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('sender_id');
+            $table->unsignedBigInteger('receiver_id');
+            $table->foreign('sender_id')->references('id')->on('users');
+            $table->foreign('receiver_id')->references('id')->on('users');
+            $table->timestamp('last_time_message')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2023_04_11_094445_create_messages_table.php
+++ database/migrations/2023_04_11_094445_create_messages_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('conversation_id')->constrained();
+            $table->unsignedBigInteger('sender_id');
+            $table->foreign('sender_id')->references('id')->on('users');
+            $table->unsignedBigInteger('receiver_id');
+            $table->foreign('receiver_id')->references('id')->on('users');
+            $table->boolean('read')->default(0)->nullable();
+            $table->text('body')->nullable();
+            $table->string('type')->nullable();
             $table->timestamps();
         });
     }
