53516241ceb05e1ce01353735c14b34e36f1ef4e
hafidzzuliesky <105539692+hafidzzuliesky@users.noreply.github.com>
2023-04-12 19:56:27 +0700

    add welcome for test

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('firstpersonprofile');
+});
+
+Route::get('/', function () {
+    return view('welcome');
 });
