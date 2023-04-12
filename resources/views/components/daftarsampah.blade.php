c1c25af07e3b5094c51818546e143d64dd2514fc
riparuk <faruqirifa@gmail.com>
2023-04-12 14:03:55 +0700

    Mengkoneksikan halaman dashboard dengan laravel pada views dan membuat beberapa component seperti navbar, kategorisampah, dan daftar sampah

---
+++ resources/views/components/daftarsampah.blade.php
@@ -0,0 +1,64 @@
+<div>
+    <div class="d-flex flex-wrap align">
+        <div class="card mt-2 mb-2 size-item">
+            <img class="size-item" src="img/kardus bekas.jpg" alt="Card image cap" />
+            <div class="card-body">
+                <h6 class="card-title text-success" style="font-size: small">
+                    <b>kardus bekas 1</b>
+                </h6>
+                <p class="card-text">Rp. 5000/kg</p>
+            </div>
+        </div>
+
+        <div class="card mt-2 mb-2 size-item">
+            <img class="size-item" src="img/kardus bekas.jpg" alt="Card image cap" />
+            <div class="card-body">
+                <h6 class="card-title text-success" style="font-size: small">
+                    <b>kardus bekas 1</b>
+                </h6>
+                <p class="card-text">Rp. 5000/kg</p>
+            </div>
+        </div>
+
+        <div class="card mt-2 mb-2 size-item">
+            <img class="size-item" src="img/kardus bekas.jpg" alt="Card image cap" />
+            <div class="card-body">
+                <h6 class="card-title text-success" style="font-size: small">
+                    <b>kardus bekas 1</b>
+                </h6>
+                <p class="card-text">Rp. 5000/kg</p>
+            </div>
+        </div>
+
+        <div class="card mt-2 mb-2 size-item">
+            <img class="size-item" src="img/kardus bekas.jpg" alt="Card image cap" />
+            <div class="card-body">
+                <h6 class="card-title text-success" style="font-size: small">
+                    <b>kardus bekas 1</b>
+                </h6>
+                <p class="card-text">Rp. 5000/kg</p>
+            </div>
+        </div>
+
+        <div class="card mt-2 mb-2 size-item">
+            <img class="size-item" src="img/kardus bekas.jpg" alt="Card image cap" />
+            <div class="card-body">
+                <h6 class="card-title text-success" style="font-size: small">
+                    <b>kardus bekas 1</b>
+                </h6>
+                <p class="card-text">Rp. 5000/kg</p>
+            </div>
+        </div>
+
+        <div class="card mt-2 mb-2 size-item">
+            <img class="size-item" src="img/kardus bekas.jpg" alt="Card image cap" />
+            <div class="card-body">
+                <h6 class="card-title text-success" style="font-size: small">
+                    <b>kardus bekas 1</b>
+                </h6>
+                <p class="card-text">Rp. 5000/kg</p>
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Waste Connect | Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="{{ asset('css/styles.css') }}" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+    <x-navbar />
+    <x-kategoripanel />
+    
+    
+    <!-- text rekomendasi postingan sampah -->
+    <div class="container-fluid bg-success">
+        <span class="text-light"><b>Rekomendasi</b></span>
+    </div>
+    <x-daftarsampah />
+    <!-- kontainer rekomendasi postingan sampah-->
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
