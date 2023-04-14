a6169338418f814dafba7c53fb1523aebcf4c806
hafidzzuliesky <105539692+hafidzzuliesky@users.noreply.github.com>
2023-04-14 09:29:10 +0700

    css style edit

--- resources/views/firstpersonprofile.blade.php
+++ resources/views/firstpersonprofile.blade.php
@@ -1,64 +1,18 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link href="style/style.css" rel="stylesheet"></link>
-        <title>WasteConnect | Another Profile</title>
-    </head>
-    <body>
-        <div class="cathegory_wrapper bg-success">
-            <img class="returnarrow_img" src="img/circle-arrow-left.svg" width="30" height="30">
-            <img class="userprofile_img" src="img/man.png" width="50" height="50">
-        </div>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>profile</title>
+  <!-- css -->
+  <link href="css/style.css" rel="stylesheet"></link>
 
-        <section class="vh-100" style="background-color: #eee;">
-            <div class="container py-5 h-80">
-              <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-md-12 col-xl-4">
-
-          
-                  <div class="card" style="border-radius: 15px;">
-                    <div class="card-body text-center">
-                      <div class="mt-3 mb-4">
-                        <img src="img/man.png"
-                          class="rounded-circle img-fluid" style="width: 100px;" />
-                      </div>
-                      <h4 class="mb-2">Hanum</h4>
-                      <p class="text-muted mb-4">Taker</p>
-
-
-
-                      <div class="text-center mt-3 mb-2">
-                        <div>
-                          <img class="" src="img/chat-speech-icon.svg" alt="" width="40" height="40">
-                          <p class="text-muted mb-0">Lihat Percakapan</p>
-                        </div>
-                        <div class="px-3">
-                          <img class="" src="img/folder-icon.svg" alt="" width="40" height="40">
-                          <p class="text-muted mb-0">Lihat Iklan</p>
-                        </div>
-                        <div>
-                          <img class="" src="img/change post.png" alt="" width="40" height="40">
-                          <p class="text-muted mb-0">Edit Profil</p>
-                        </div>
-                        <button type="button" class="btn btn-primary btn-rounded btn-lg mt-3">
-                            Logout
-                        </button>
-                      </div>
-
-                    </div>
-                  </div>
-          
-                </div>
-              </div>
-            </div>
-        </section>
-
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </body>
+</head>
+<body>
+  <div class="profile-box">
+    <img src="img/man.png" class="photo">
+  </div>
+  
+</body>
 </html>
