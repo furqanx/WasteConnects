c60ed10528dbcf619c63ee5ae6d7515cf35df6ef
furqanx <alghifarifurqan03@gmail.com>
2023-04-21 09:54:57 +0700

    modifying routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -31,45 +31,60 @@ Route::get('/', function () {
 
 // Auth::routes();
 
+
 /** route ke halaman home */
 Route::get('/home', [PostController::class, 'showall']);
 
 /** route ke halaman login  */
 Route::get('/login', [LoginController::class, 'index']);
 
-/** route ke halaman login  */
-Route::get('/register', [RegisterController::class, 'index']);
 
+
+
+/** route ke halaman register  */
+Route::get('/register', [RegisterController::class, 'index']);
+/** route untuk menyimpan data registrasi */
+Route::post('/register/store', [RegisterController::class, 'store']);
+/** DEBUG untuk menyimpan data registrasi */
+Route::post('/register/debug', [RegisterController::class, 'debug']);
+
+
+
+
+/** route ke halaman form mengupload barang */
+Route::get('/uploadproduct', [UploadController::class, 'index']);
+
+/** route untuk menyimpan data product */
+Route::post('/uploadproduct/store', [UploadController::class, 'store']);
+
+/** DEBUG route untuk menyimpan data upload data product */
+Route::post('/uploadproduct/debug', [UploadController::class, 'debug']);
+
+
+
+
+/** TUNDA */
 /** route ke halaman profile */
 Route::get('/profile/{id}', [PostController::class, 'show_byId']);
+/** DEBUG route untuk mengedit profil user */
+Route::post('/profile/proses', [PostController::class, 'debug']);
 
+
+/** TUNDA */
 /** route untuk melihat card postingan */
 Route::get('/post/{id}', [PostController::class, 'show_byId']);
 
-/** route untuk mengupload barang  TUNDA TERLEBIH DAHULU */
-Route::get('/uploadproduct', [UploadController::class, 'upload']);
-Route::post('/uploadproduct/proses', [UploadController::class, 'proses_upload']);
 
+/** route untuk halaman chat */
 /** livewire routes  */
 Route::get('/users',CreateChat::class)->name('users');
 Route::get('/chat{key?}',Main::class)->name('chat');
 
-
-
-/** debugging */
-// Route::get('/profile', function () {
-//     return view('profile');
-// });
-
 // /** route untuk melihat barang berdasarkan kategori */  
 // Route::get('/homebycategory/{category}', [PostController::class, 'show_byCategory']);
 
-
-
-
 // Route::get('user', [UserController::class, 'index']);
 // Route::get('post', [PostController::class, 'index']);
-
 
 // Auth::routes();
 
