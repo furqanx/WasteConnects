8ecf8c4ed488af828f8b2fc99a4163fc5e6186fb
riparuk <faruqirifa@gmail.com>
2023-04-30 04:07:11 +0700

    add new layout that only works for bootstrap

---
+++ resources/views/layouts/app2.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Waste Connect</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+         <!-- Fonts -->
+         <link rel="preconnect" href="https://fonts.bunny.net">
+         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+ 
+    </head>
+    {{-- <body class="font-sans antialiased"> --}}
+    <body>
+
+        <!-- komponen navbar wasteconnect -->
+        <header>
+            @include('layouts.navigation2')
+        </header>
+
+        @yield('content')
+        
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/script.js') }}"></script>
+    </body>
+</html>
