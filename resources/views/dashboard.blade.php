052364251156b33618e6b942681e269df9688d6e
riparuk <faruqirifa@gmail.com>
2023-04-30 03:57:30 +0700

    modified after install breeze starter kit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,52 +1,17 @@
-<<<<<<< HEAD
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
-=======
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Waste Connect | Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link href="{{ asset('css/styles.css') }}" type="text/css" rel="stylesheet">
-</head>
-
-<body>
-    <x-navbar />
-    <x-kategoripanel />
-    
-    
-    <!-- text rekomendasi postingan sampah -->
-    <div class="container-fluid bg-success">
-        <span class="text-light"><b>Rekomendasi</b></span>
-    </div>
-    <x-daftarsampah />
-    <!-- kontainer rekomendasi postingan sampah-->
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
->>>>>>> 3fb9a2d1b3a7ca3e6a5870a7379806bf344f2da6
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,31 +3,34 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Waste Connect</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Styles -->
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-         <!-- Fonts -->
-         <link rel="preconnect" href="https://fonts.bunny.net">
-         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
- 
-         <!-- Scripts -->
-         {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    </head>
-    {{-- <body class="font-sans antialiased"> --}}
-    <body>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- komponen navbar -->
-        <nav>
-            @include('components.navbar')
-        </nav>
-
-        @yield('content')
-        
         <!-- Scripts -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-        <script src="{{ asset('js/script.js') }}"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
     </body>
 </html>
