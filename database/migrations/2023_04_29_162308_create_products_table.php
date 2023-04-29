d446bae18cde9f5a514693307b6822b2c462db7e
riparuk <faruqirifa@gmail.com>
2023-04-30 04:01:02 +0700

    change post to product table

---
+++ database/migrations/2023_04_29_162308_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 25);
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->string('location');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
