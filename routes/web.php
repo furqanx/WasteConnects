dc290109c305a92c1d7f87cb660f645bc6c8df67
riparuk <faruqirifa@gmail.com>
2023-04-30 04:10:12 +0700

    simplify with breeze starter kit auth and edit some

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
-
-use App\Http\Livewire\Chat\CreateChat;
-use App\Http\Livewire\Chat\Main;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,58 +15,24 @@ use App\Http\Livewire\Chat\Main;
 |
 */
 
-// Auth::routes();
-
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', [ProductController::class, 'index'])->name('products');
 
-/** Route ke halaman home */
-Route::get('/home', [HomeController::class, 'index'])->name('home');  
-/** Route untuk melihat postingan berdasarkan kategori */
-Route::get('/home/{category}', [CategoryController::class, 'index'])->name('home.category');  
-/** Route untuk search bar */
-Route::get('/search', [PostController::class, 'search'])->name('post.search');
+/*
+-pada Laravel Breeze, rute untuk halaman login dan register dapat ditemukan di dalam file routes/auth.php.
+-route setelah login dapat diubah pada RouteServiceProvider::HOME
+*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-/** Route ke halaman form mengupload barang */
-Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
-/** Route untuk menyimpan data product */
-Route::post('/post', [PostController::class, 'store'])->name('post.store');
-/** Route untuk melihat card postingan */
-Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
-/** DEBUG */
-Route::post('/post/debug', [PostController::class, 'debug'])->name('post.debug');
-
-
-/** Route untuk menampilkan profile */  
-Route::get('/profile/show', [ProfileController::class, 'show'])->name('profile.show');  
-/** Route untuk menampilkan form untuk mengedit profile tertentu. */
-Route::get('/profile/{id}/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-/** Route untuk Memperbarui data profile tertentu. */
-Route::put('/profile/{id}', [ProfileController::class, 'update'])->name('profile.update');
-
-
-
-
-/** route ke halaman login  */
-Route::get('/login', [LoginController::class, 'index']);
-Route::post('/login/authenticate', [LoginController::class, 'authenticate']); 
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-
-
-/** route ke halaman register  */
-Route::get('/register', [RegisterController::class, 'index']);
-/** route untuk menyimpan data registrasi */
-Route::post('/register/store', [RegisterController::class, 'store']);
-/** DEBUG untuk menyimpan data registrasi */
-Route::post('/register/debug', [RegisterController::class, 'debug']);
-
-
-
-
-/** route untuk halaman chat */
-/** livewire routes  */
-Route::get('/users', [CreateChat::class, 'render'])->name('users');
-Route::get('/chat{key?}', [Main::class, 'render'])->name('chat');
+require __DIR__.'/auth.php';
