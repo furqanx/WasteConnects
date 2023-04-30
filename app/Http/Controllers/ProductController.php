8744eea2cea986bfbe2d131367454ea5d769cdce
malikkamal12 <malik21@mhs.unsyiah.ac.id>
2023-04-30 22:35:10 +0700

    feat: menambahkan fitur untuk mengarahkan user ke halaman tambah product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,13 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('uploadproduct');
+    }
+
     public function index(Request $request)
     {
         $products = Product::all();
