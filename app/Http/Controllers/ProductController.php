a10ededfb13d5dbfe422d0c1292ceb702cb47007
riparuk <faruqirifa@gmail.com>
2023-05-01 18:29:15 +0700

    menambah method untuk menampilkan per produk dan produk saya

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,20 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\User;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
-    public function create()
-    {
-        return view('uploadproduct');
-    }
 
     public function index(Request $request)
     {
         $products = Product::all();
         $categories = Category::all();
         return view('main', compact('products', "categories"));
+    }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('product.show', compact('product'));
+    }
+
+    /**
+     * method untuk menampilkan semua produk yang telah di posting oleh user yang telah login
+     */
+    public function myproducts()
+    {
+        $products = Auth::user()->products;
+        return view('product.myproduct', compact('products'));
     }
 }
