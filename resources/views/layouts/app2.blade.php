e355df60ceb0435c9d038b23ddc19c066b5c0e38
riparuk <faruqirifa@gmail.com>
2023-05-02 13:10:38 +0700

    fix href css

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -7,7 +7,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
         <!-- Styles -->
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 
          <!-- Fonts -->
          <link rel="preconnect" href="https://fonts.bunny.net">
