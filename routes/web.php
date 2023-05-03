53c9a586a3008a2823ff7a686f994b4d1a6682e9
hafidzzuliesky <105539692+hafidzzuliesky@users.noreply.github.com>
2023-05-03 22:14:27 +0700

    route  manipulation

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,48 @@ use App\Http\Controllers\UserController;
 |
 */
 
-
-Route::get('/profile', function () {
-    return view('firstpersonprofile');
-});
-
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function () {
-    return view('addproduct');
+
+// Halaman utama yang menampilkan semua produk
+Route::get('/', [ProductController::class, 'index'])->name('products');
+// Mendapatkan barang berdasarkan category
+Route::get('/category/{category}', [ProductController::class, 'searchByCategory'])->name('products.searchByCategory');
+// Mencari berdasarkan nama barang
+Route::get('/search', [ProductController::class, 'search'])->name('products.search');
+// Melihat info profile berdasarkan id
+Route::get('/user/{id?}', [UserController::class, 'show'])->name('user.show');
+
+// Untuk Melihat Products dari user tertentu.
+Route::get('/user/{id}/products', [UserController::class, 'userProducts'])->name('user.products');
+
+// Melihat info product berdasarkan id
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
+
+
+/*
+-pada Laravel Breeze, rute untuk halaman login dan register dapat ditemukan di dalam file routes/auth.php.
+-route setelah login dapat diubah pada RouteServiceProvider::HOME
+*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/createProduct', [ProductController::class, 'create'])->name('product.create'); //route untuk pergi ke halaman menambahkan product
+    Route::post('/createProduct', [ProductController::class, 'store'])->name('product.store');  //route untuk mengirim data ke server dengan method POST
+    Route::get('/editProduct/{id}', [ProductController::class, 'edit'])->name('product.edit');  //route untuk pergi ke halaman edit product
+    Route::patch('/editProduct/{id}', [ProductController::class, 'update'])->name('product.update');    //route untuk mengirim editan data product 
+    
+    // Untuk Melihat Product Sendiri
+    Route::get('/myproducts', [ProductController::class, 'myproducts'])->name('myproducts');
+    
 });
 
+require __DIR__ . '/auth.php';
