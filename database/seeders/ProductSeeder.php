27d98ff5cc87e6a94ed188934d407a0feaf31fa0
riparuk <faruqirifa@gmail.com>
2023-05-06 19:38:40 +0700

    untuk tujuan seeder gambar produk saat seeder dijalankan

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Product;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class ProductSeeder extends Seeder
 {
@@ -16,9 +18,23 @@ class ProductSeeder extends Seeder
         $users = \App\Models\User::all(); // ambil semua user
         // buat 5 produk untuk setiap user
         $users->each(function ($user) {
-            Product::factory()->count(5)->create([
-                'user_id' => $user->id
-            ]);
+            Product::factory()
+                ->count(5)
+                ->create([
+                    'user_id' => $user->id,
+                ]);
         });
+
+        /**
+         * Copy no-image for product from seeder.
+         */
+        $filePath = public_path('product_images/no-image.png');
+        $storagePath = 'public/product_images/no-image.png';
+
+        // create the directory if it doesn't exist
+        Storage::makeDirectory(dirname($storagePath));
+
+        // copy the file to the storage path
+        File::copy($filePath, Storage::path($storagePath));
     }
 }
