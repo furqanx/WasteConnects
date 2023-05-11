fd5e2e984d1e5a2e4cc497e672333607dc0d41f5
malikkamal12 <malik21@mhs.unsyiah.ac.id>
2023-05-11 21:32:06 +0700

    feat: menambahkan team section

--- resources/views/landing.blad.php
+++ resources/views/landing.blad.php
@@ -587,4 +587,89 @@
         </div>
     </section><!-- End Portfolio Section --> --}}
 
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg">
+        <div class="container">
+
+            <div class="section-title">
+                <h2>Team</h2>
+                {{-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit
+                    sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
+                    Quia fugiat sit in iste officiis commodi quidem hic quas.</p> --}}
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                    <div class="member">
+                        <img src="{{ asset('img/img-furqan-2.JPG') }}" alt="">
+                        <h4>Furqan Al Ghifari Zulva</h4>
+                        <span>Students</span>
+                        <p>
+                            Mewujudkan Ide menjadi Realitas Digital
+                        </p>
+                        {{-- <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div> --}}
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                    <div class="member">
+                        <img src="{{ asset('img/img-malik-2.jpeg') }}" alt="">
+                        <h4>T Malik Kamal</h4>
+                        <span>Students</span>
+                        <p>
+                            Menciptakan Pengalaman Pengguna yang Ramah
+                        </p>
+                        {{-- <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div> --}}
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                    <div class="member">
+                        <img src="{{ asset('img/img-hafidz-2.jpeg') }}" alt="">
+                        <h4>M Hafidz Zuliesky</h4>
+                        <span>Students</span>
+                        <p>
+                            Membangun Komunitas Online dengan Satu Klik
+                        </p>
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                    <div class="member">
+                        <img src="{{ asset('img/img-rifa.jpeg') }}" alt="">
+                        <h4>Rifa Faruqi</h4>
+                        <span>Students</span>
+                        <p>
+                            Menganyam Cerita Digital dengan Desain
+                        </p>
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section><!-- End Team Section -->
+
     @endsection
